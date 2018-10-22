a3c6ea4de4b6f17cd9378ab599244b81cbb0fb95
COR Students <corstudents@Matts-MBP-76.localdomain>
2018-10-22 12:16:26 -0400

    Finished assessment

---
+++ Assessment1.js
@@ -0,0 +1,44 @@
+//Number 1
+let apple = "apple";
+let five = 5;
+let isSkyBlue = true;
+let pi = 3.14;
+
+//Number 2
+let trueOrFalse = true;
+if (trueOrFalse === true) {
+    return "1";
+} else {
+    return "2";
+}
+
+//Number 3
+for (var i=0; i<=100; i++) {
+    if (i % 2 === 0) {
+        console.log(i + " ")
+    }
+}
+ 
+//Number 4
+i = 0;
+let iAmTwenty = true;
+while (i<=20) {
+    console.log("Hello");
+    i++;
+} while (i<20) {
+return "False";
+}
+
+//Number 5
+function Name(firstName, lastName) {
+    if (firstName + " " + lastName === "Harland Gunn") {
+        return "True"
+    } else {
+        return "False"
+    }
+}
+
+// Bonus Question:
+// The acronym D.R.Y. stands for Don't Repeat Yourself.😎
+
+
